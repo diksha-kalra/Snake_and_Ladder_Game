f5a5ad53e51bd0cf663e75fd752b7f011a9c77b4
diksha-kalra <kalradiksha11@gmail.com>
2020-09-25 02:50:39 +0530

    Added : Play game till 100th position reached

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -5,6 +5,7 @@ public class SnakeLadder {
 	public static final int NO_PLAY=0;
 	public static final int LADDER=1;
 	public static final int SNAKE=2;
+	public static final int WINNING_POS=100;
 
 	public static void main(String[] args) {
 
@@ -13,7 +14,9 @@ public class SnakeLadder {
 		
 		//Initialising the postion 
 		int pos=0;
-
+		
+		//Continue to play until final position reached
+		while(pos!=WINNING_POS) {
 		//Generating random between 1 to 6
 		int number_on_dice= 1 + (int)(Math.random() * ((6 - 1) + 1));
 		System.out.println("random number is "+number_on_dice );
@@ -35,8 +38,12 @@ public class SnakeLadder {
 				
 			case SNAKE :
 				pos-=number_on_dice;
+				if(pos<0) {
+					pos=0;
+				}
 				System.out.println("Snake Bite, new position is "+pos);
 				break;
+		}
 		}
 	}
 }
