1fec57cb1d167003897f0e38056c23ce7ad12caf
diksha-kalra <kalradiksha11@gmail.com>
2020-09-25 02:39:57 +0530

    Added : Random generation of options

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,6 +1,11 @@
 package com.snakenladder;
 
 public class SnakeLadder {
+	//Constants Declaration
+	public static final int NO_PLAY=0;
+	public static final int LADDER=1;
+	public static final int SNAKE=2;
+
 	public static void main(String[] args) {
 
 		//Printing the game name
@@ -12,9 +17,27 @@ public class SnakeLadder {
 		//Generating random between 1 to 6
 		int number_on_dice= 1 + (int)(Math.random() * ((6 - 1) + 1));
 		System.out.println("random number is "+number_on_dice );
-
-
+		
+		//Generating options
+		int option = (int) Math.floor(Math.random()*10)%3;
+		System.out.println("game option "+option);
+		
+		switch (option) {
+			case NO_PLAY :
+				pos+=0;
+				System.out.println("No Play, my postion "+pos);
+				break;
+		
+			case LADDER :
+				pos+=number_on_dice;
+				System.out.println("Up on the ladder, new position is "+pos);
+				break;
+				
+			case SNAKE :
+				pos-=number_on_dice;
+				System.out.println("Snake Bite, new position is "+pos);
+				break;
+		}
 	}
-	
 }
 		
