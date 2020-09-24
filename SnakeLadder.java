e1067aedc707ba4d8a670c7be20aec59c9073639
diksha-kalra <kalradiksha11@gmail.com>
2020-09-25 02:09:19 +0530

    Added : Snake and Ladder Printed

---
+++ SnakeLadder.java
@@ -0,0 +1,11 @@
+package com.snakenladder;
+
+public class SnakeLadder {
+	public static void main(String[] args) {
+		//printing the game name
+		System.out.println("Snake and Ladder Game \n");
+		
+	}
+	
+}
+		
