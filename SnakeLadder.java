8e9e052afab7449cb942aa806b3bb66a316bc4cf
diksha-kalra <kalradiksha11@gmail.com>
2020-09-25 03:20:31 +0530

    Added : snake and ladder two player game

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -6,51 +6,115 @@ public class SnakeLadder {
 	public static final int LADDER=1;
 	public static final int SNAKE=2;
 	public static final int WINNING_POS=100;
-
-	public static void main(String[] args) {
-
-		//Printing the game name
-		System.out.println("Snake and Ladder Game \n");
-		
-		//Initialising the postion 
-		int pos=0;
-		int dice_rolls=0;
-
-		//Continue to play until final position reached
-		while(pos!=WINNING_POS) {
-		//Generating random between 1 to 6
-		int number_on_dice= 1 + (int)(Math.random() * ((6 - 1) + 1));
-		System.out.println("random number is "+number_on_dice );
-		dice_rolls++;
-		
-		//Generating options
-		int option = (int) Math.floor(Math.random()*10)%3;
-		System.out.println("game option "+option);
+	
+	//Method which generates the new position of player based on random option, players previous position and number on dice 
+	public static int randomOption(int option, int pos, int number_on_dice) {
 		
 		switch (option) {
 			case NO_PLAY :
 				pos+=0;
-				System.out.println("No Play, my postion "+pos);
+				System.out.println("No Play, stay on same postion: "+pos);
 				break;
 		
 			case LADDER :
-				pos+=number_on_dice;
-				if(pos>WINNING_POS) {
-					pos-=number_on_dice;
+				if(pos+number_on_dice>WINNING_POS) {
+					pos+=0;
+					System.out.println("Number exceeds winning position, stay at position "+pos);
 				}
-				System.out.println("Up on the ladder, new position is "+pos);
+				else {
+					pos+=number_on_dice;
+					System.out.println("Up on the ladder, new position: "+pos);
+				}
 				break;
 				
 			case SNAKE :
-				pos-=number_on_dice;
-				if(pos<0) {
-					pos=0;
-				}
-				System.out.println("Snake Bite, new position is "+pos);
+					pos-=number_on_dice;
+					if(pos<0) {
+						pos=0;
+					}
+					System.out.println("Snake Bite go down,  new position: "+pos);
 				break;
-		}
-		}
-		System.out.println("Number of dice rolls to win the game "+dice_rolls);
-	}
-}
+				}
 		
+				return pos;
+	}
+	
+	public static void main(String[] args) {
+		
+		//Printing the game name
+		System.out.println("Snake and Ladder Game \n");
+		
+		//Variables
+		int pos_player_1=0;
+		int pos_player_2=0;
+		int dice_rolls_p1=0;
+		int dice_rolls_p2=0;
+		int option_p1, option_p2;
+		int number_on_dice_p1,number_on_dice_p2;
+		
+		while(pos_player_1!=WINNING_POS || pos_player_2!=WINNING_POS) {
+			
+		//Player 1 ----------------------------------
+		//Generating random between 1 to 6 for player1	
+		number_on_dice_p1= 1 + (int)(Math.random() * ((6 - 1) + 1));
+		System.out.println("Number on Dice of Player 1 : "+number_on_dice_p1 );
+		dice_rolls_p1++;
+		
+		//Generating random option for player 1
+		option_p1 = (int) Math.floor(Math.random()*10)%3;
+		System.out.println("Game Option : "+option_p1);
+		pos_player_1= randomOption(option_p1,pos_player_1,number_on_dice_p1);
+		
+		//Condition to check if option is ladder and giving another chance to player 1
+		if(option_p1==LADDER && pos_player_1!= WINNING_POS ) {
+			
+			number_on_dice_p1= 1 + (int)(Math.random() * ((6 - 1) + 1));
+			System.out.println("Roll Dice Again Player 1 : "+number_on_dice_p1 );
+			dice_rolls_p1++;
+			option_p1 = (int) Math.floor(Math.random()*10)%3;
+			System.out.println("Game Option : "+option_p1);
+			pos_player_1= randomOption(option_p1,pos_player_1,number_on_dice_p1);
+		}
+		
+		//Condition to check if Player 1 reaches first
+		if(pos_player_1== WINNING_POS) {
+			System.out.println(" Winner ---- Player1 : Number of dice rolls to win the game "+dice_rolls_p1);
+			break;
+		}
+		
+		//Player 2 -------------------------------------------
+		//Generating random between 1 to 6 for player1	
+		number_on_dice_p2= (int)(Math.random()*(6-1+1)+1);
+		System.out.println("Number on Dice of Player 2 : "+number_on_dice_p2 );
+		dice_rolls_p2++;
+		
+		//Generating random option for player 2
+		option_p2 = (int) Math.floor(Math.random()*10)%3;
+		System.out.println("Game Option : "+option_p2);
+		pos_player_2= randomOption(option_p2,pos_player_2,number_on_dice_p2);
+		
+		//Condition to check if option is Ladder and giving another chance to player 2
+		if(option_p2==LADDER && pos_player_2!= WINNING_POS) {
+			
+			number_on_dice_p2= 1 + (int)(Math.random() * ((6 - 1) + 1));
+			System.out.println("Roll Dice Again player 2 : "+number_on_dice_p2 );
+			dice_rolls_p2++;
+			
+			option_p2 = (int) Math.floor(Math.random()*10)%3;
+			System.out.println(" Game Option : "+option_p2);
+			pos_player_2= randomOption(option_p2,pos_player_2,number_on_dice_p2);
+		}
+		
+		//Condition to check if Player 2 wins
+		if(pos_player_2== WINNING_POS) {
+			System.out.println(" Winner ---- Player2 : Number of dice rolls to win the game "+dice_rolls_p2);
+			break;
+		}
+		
+		}
+		
+	}
+
+}
+
+
